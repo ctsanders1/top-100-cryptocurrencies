d132f78942da9c73e3be6fe94c66bad72e2dc316
dankoknad <dankomilutinovic@gmail.com>
2018-08-02 00:56:39 +0200

    writting snapshot tests for a RefreshCurrenciesBtn (connected and unconnected)

--- src/containers/RefreshCurrenciesBtn.js
+++ src/containers/RefreshCurrenciesBtn.js
@@ -4,14 +4,14 @@ import { bindActionCreators } from 'redux'
 import PropTypes from 'prop-types'
 import { fetchData } from '../redux/actions'
 
-const RefreshCurrenciesBtn = ({ fetchData }) => (
+export const RefreshCurrenciesBtn = ({ fetchData }) => (
   <button className="btn btn-outline-primary" onClick={fetchData}>
     Get a fresh data
   </button>
 )
 
 RefreshCurrenciesBtn.propTypes = {
-  fetchData: PropTypes.func.isRequired
+  fetchData: PropTypes.func
 }
 
 export default connect(
