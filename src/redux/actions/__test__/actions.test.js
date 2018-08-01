a6e14716c32d492467d5eec950577e412249cbd9
dankoknad <dankomilutinovic@gmail.com>
2018-08-01 19:08:29 +0200

    writting test for a fetchDataError action creator

--- src/redux/actions/__test__/actions.test.js
+++ src/redux/actions/__test__/actions.test.js
@@ -1,8 +1,33 @@
-import { updateFilter, UPDATE_FILTER } from '../'
+import {
+  updateFilter,
+  fetchDataError,
+  UPDATE_FILTER,
+  FETCH_DATA_ERROR
+} from '../'
 
 test('updateFilter action creator genetates correct object', () => {
   const actual = updateFilter('EUR'),
     expected = { type: UPDATE_FILTER, payload: 'EUR' }
 
   expect(actual).toEqual(expected)
+})
+
+describe('multiple tests against fetchDataError action creator', () => {
+  it('fetchDataError action creator genetates correct object on http success', () => {
+    const actual = fetchDataError(false, ''),
+      expected = { type: FETCH_DATA_ERROR, payload: false, err: '' }
+
+    expect(actual).toEqual(expected)
+  })
+
+  it(' action creator genetates correct object on http error', () => {
+    const actual = fetchDataError(true, 'something went wrong'),
+      expected = {
+        type: FETCH_DATA_ERROR,
+        payload: true,
+        err: 'something went wrong'
+      }
+
+    expect(actual).toEqual(expected)
+  })
 })
