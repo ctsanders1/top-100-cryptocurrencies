721fa7b02c4a71659d9300481aeadacfd940406f
dankoknad <dankomilutinovic@gmail.com>
2018-08-01 22:17:27 +0200

    adding static typechecking of the CurrencyList container with PropTypes

--- src/containers/CurrencyList.js
+++ src/containers/CurrencyList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import CurrencyListItem from '../components/CurrencyListItem'
+import PropTypes from 'prop-types'
 
 const CurrencyList = ({ filter, data }) => (
   <div>
@@ -19,5 +20,10 @@ const CurrencyList = ({ filter, data }) => (
     </ul>
   </div>
 )
+
+CurrencyList.propTypes = {
+  data: PropTypes.object.isRequired,
+  filter: PropTypes.string.isRequired
+}
 
 export default connect(state => state)(CurrencyList)
