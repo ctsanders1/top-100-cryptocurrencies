fba636234e0693b537f9c0165eb5aed388c4a517
dankoknad <dankomilutinovic@gmail.com>
2018-08-02 14:34:40 +0200

    converting CurrencyList to class component, adding shouldComponentUpdate lifecycle method, block redundant rendering

--- src/containers/CurrencyList.js
+++ src/containers/CurrencyList.js
@@ -1,28 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import CurrencyListItem from '../components/CurrencyListItem'
 import PropTypes from 'prop-types'
 import RefreshCurrenciesBtn from './RefreshCurrenciesBtn'
 
-const CurrencyList = ({ filter, data }) =>
-  console.log('CurrencyList') || (
-    <div>
-      <RefreshCurrenciesBtn />
-      <ul className="list-group my-5">
-        {Object.keys(data).length ? (
-          Object.keys(data).map(itemKey => (
-            <CurrencyListItem
-              key={itemKey}
-              item={data[itemKey]}
-              filter={filter}
-            />
-          ))
-        ) : (
-          <li>Loading...</li>
-        )}
-      </ul>
-    </div>
-  )
+class CurrencyList extends Component {
+  shouldComponentUpdate(nextProps) {
+    if (
+      this.props.data[1] !== undefined &&
+      this.props.filter === nextProps.filter &&
+      this.props.data[1].last_updated === nextProps.data[1].last_updated
+    ) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  render() {
+    const { data, filter } = this.props
+    console.log('CurrencyList')
+    return (
+      <div>
+        <RefreshCurrenciesBtn />
+        <ul className="list-group my-5">
+          {Object.keys(data).length ? (
+            Object.keys(data).map(itemKey => (
+              <CurrencyListItem
+                key={itemKey}
+                item={data[itemKey]}
+                filter={filter}
+              />
+            ))
+          ) : (
+            <li>Loading...</li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
 
 CurrencyList.propTypes = {
   data: PropTypes.object.isRequired,
