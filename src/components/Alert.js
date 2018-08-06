e9265a181c8e9c4c179299576c22f474516e288e
dankoknad <dankomilutinovic@gmail.com>
2018-08-06 13:17:04 +0200

    CurrencyDetail - integrating Alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Alert = ({ type, msg }) => (
+const Alert = ({ type, children }) => (
   <div className={`alert alert-${type}`} role="alert">
-    {msg}
+    {children}
   </div>
 )
 
